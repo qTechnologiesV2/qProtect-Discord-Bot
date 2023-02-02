cdbc1f090bcaad690102db2fcbeacbc84893bd9d
TheRealShitDev <exslodingdogs@gmail.com>
2023-02-02 20:03:01 +0000

    Fixed errors and Updated QProtect Lite
    + Added a custom config validator to stop the bot from breaking if someone uses an invalid config
    + Updated to the latest version of QProtect Lite
    + Updated the default config to work with QProtect Lite

--- commands/obfuscate/obfuscate.js
+++ commands/obfuscate/obfuscate.js
@@ -3,7 +3,10 @@ const https = require(`https`);
 const exec = require('child_process').exec;
 const fs = require(`fs`);
 
+const YAML = require('yaml')
+
 let running = false;
+let error = false;
 
 
 const watermark = `𝗤𝗣𝗿𝗼𝘁𝗲𝗰𝘁 𝗢𝗯𝗳𝘂𝘀𝗰𝗮𝘁𝗼𝗿 𝗖𝗼𝗻𝘀𝗼𝗹𝗲\n\n`
@@ -41,7 +44,7 @@ async function obfuscate(config) {
 
 }
 
-let console = ``;
+let console_log = ``;
 
 async function discord_log(message, interaction, percentage) {
 
@@ -53,10 +56,33 @@ async function discord_log(message, interaction, percentage) {
         .setFooter({ text: 'QProtect Bot by aSwitch', iconURL: `https://mdma.dev/data/assets/logo/Icon.png` })
         .setTimestamp()
 
-    console += `-> ` + message + `\n`;
-    embed.setDescription("```" + console + "```");
+    console_log += `-> ` + message + `\n`;
+    embed.setDescription("```" + console_log + "```");
     await interaction.editReply({ embeds: [embed] });
 }
+
+
+function delete_tmp_files(interaction) {
+    discord_log(`Deleting Files...`, interaction);
+    try {
+        if (fs.existsSync(`./tmp/input.jar`)) {
+            fs.unlinkSync(`./tmp/input.jar`);
+        }
+
+        if (fs.existsSync(`./tmp/output.jar`)) {
+            fs.unlinkSync(`./tmp/output.jar`);
+        }
+
+        if (fs.existsSync(`./tmp/config.yml`)) {
+            fs.unlinkSync(`./tmp/config.yml`);
+        }
+    } catch (err) {
+        console.log(err)
+    }
+    discord_log(`Deleted Files!`, interaction);
+}
+
+let lastObfuscate;
 
 module.exports = {
     name: `obfuscate`,
@@ -74,7 +100,7 @@ module.exports = {
     async execute(interaction, client) {
 
         await interaction.deferReply({
-            fetchReply: true
+            fetchReply: false
         });
 
         const embed = new EmbedBuilder()
@@ -83,50 +109,106 @@ module.exports = {
             .setFooter({ text: 'QProtect Bot by aSwitch', iconURL: `https://mdma.dev/data/assets/logo/Icon.png` })
             .setTimestamp()
 
+
+
+        /**
+         * Checking if the bot is trying to obfuscate a file already.
+         */
         if (running) {
-            embed.setDescription(`> Only one file can be obfuscated at a time.`)
-            await interaction.editReply({ embeds: [embed] });
-            return;
+            if (new Date().getTime() - lastObfuscate > 15000) {
+                running = false;
+                delete_tmp_files(interaction)
+            } else {
+                embed.setDescription(`> Only one file can be obfuscated at a time.`)
+                await interaction.editReply({ embeds: [embed] });
+                return;
+            }
         } else {
             running = true;
         }
+
+        lastObfuscate = new Date().getTime();
 
 
         const file = interaction.options.getAttachment('jar')
         const config = interaction.options.getAttachment('config')
 
+        /**
+         * Downloading the input.jar file
+         */
+
         discord_log(`Downloading File...`, interaction);
         await download(file.url, `input.jar`)
         discord_log(`Downloaded File!`, interaction);
+
+
+        /**
+         * Downloading the custom config and pre checking the config.
+         */
 
         if (config) {
             discord_log(`Downloading Config...`, interaction);
             await download(config.url, `config.yml`)
             discord_log(`Downloaded Config!`, interaction);
+
+            discord_log(`Checking Custom Config...`, interaction);
+
+            try {
+                const config_file = fs.readFileSync(`./tmp/config.yml`, 'utf8');
+                const yaml_config = YAML.parse(config_file);
+
+                if (yaml_config.file.input !== "./tmp/input.jar") {
+                    discord_log("Custom Config Failed! Set the \"input\" value to ./tmp/input.jar inside your config", interaction);
+                    delete_tmp_files(interaction)
+                    running = false;
+                    console_log = ``;
+                    return;
+                }
+
+                if (yaml_config.file.output !== "./tmp/output.jar") {
+                    discord_log("Custom Config Failed! Set the \"output\" value to ./tmp/output.jar inside your config", interaction);
+                    delete_tmp_files(interaction)
+                    running = false;
+                    console_log = ``;
+                    return;
+                }
+
+                discord_log(`Custom Config Passed!`, interaction);
+            } catch (error) {
+                discord_log(`Detected Invalid Config!`, interaction);
+                delete_tmp_files(interaction)
+                running = false;
+                console_log = ``;
+                return;
+            }
         }
+
+
+
+        /**
+         * Obfuscating the input.jar file.
+         */
 
         discord_log(`Obfuscating File...`, interaction);
         await obfuscate(config)
         discord_log(`Obfuscated File!`, interaction);
 
+
+
+        /**
+         * Sending the obfuscated file into the chat
+         */
         await interaction.editReply({ files: [`./tmp/output.jar`] });
 
-        discord_log(`Deleting Files...`, interaction);
 
-        try {
-            fs.unlinkSync(`./tmp/input.jar`);
-            fs.unlinkSync(`./tmp/output.jar`);
-            if (config) {
-                fs.unlinkSync(`./tmp/config.yml`);
-            }
 
-            discord_log(`Deleted Files!`, interaction);
-        } catch (err) {
-            discord_log(err);
-        }
+        /**
+         * Deleting all tmp files
+         */
+        delete_tmp_files(interaction)
 
-        console = ``;
 
+        console_log = ``;
         running = false;
     },
 }
